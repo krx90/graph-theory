e3bcfae0626dc00caae8b3ca4d2d52544e6b7398
krx90 <118390093+krx90@users.noreply.github.com>
2024-11-12 20:00:41 +0100

    zweite aufgabe geloest

--- graph.py
+++ graph.py
@@ -3,10 +3,21 @@ adjazenzmatrix = [[0, 1, 0, 0], [0, 1, 1, 1], [1, 1, 0, 0], [0, 0, 0, 0]]
 
 def existiertKnoten(name):
     if name in knotenliste:
+        print("True")
         return True
     else:
+        print("False")
+        return False
+
+existiertKnoten('A')
+
+def existiertKante(Startknoten, Endknoten):
+    if adjazenzmatrix[knotenliste.index(Startknoten)][knotenliste.index(Endknoten)] == 1:
+        print("True")
+        return True
+    else: 
+        print("False")
         return False
     
-knoten1 = input("Knoten 1? >>> ")
+existiertKante('A', 'B')
 
-print(existiertKnoten(knoten1))
